8757fb3adad7e718226cbf36813352297e08c71e
Robert Hung <rph.hung@gmail.com>
2020-04-30 14:17:55 +0800

    Exercise-00

--- exercises/exercise-00/index.ts
+++ exercises/exercise-00/index.ts
@@ -26,21 +26,27 @@ Run this exercise:
 
 */
 
-const users: unknown[] = [
-    {
-        name: 'Max Mustermann',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    }
+interface Users {
+  name: string;
+  age: number;
+  occupation: string;
+}
+
+const users: Users[] = [
+  {
+    name: 'Max Mustermann',
+    age: 25,
+    occupation: 'Chimney sweep',
+  },
+  {
+    name: 'Kate Müller',
+    age: 23,
+    occupation: 'Astronaut',
+  },
 ];
 
-function logPerson(user: unknown) {
-    console.log(` - ${chalk.green(user.name)}, ${user.age}`);
+function logPerson(user: Users) {
+  console.log(` - ${chalk.green(user.name)}, ${user.age}`);
 }
 
 console.log(chalk.yellow('Users:'));
